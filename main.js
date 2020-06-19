a35ded2d79d6a226611d1038eeab8fb834759eac
Mark Nelson <mnelson400@comcast.net>
2020-06-19 13:54:21 -0500

    Initial Commit

---
+++ main.js
@@ -0,0 +1,25 @@
+// Assessment: DOM Methods Practice
+
+
+// Challenge:
+
+// 1. Your first task is to use DOM methods to re-create 
+//  the DOM tree below using only JavaScript.
+
+// 2. Now, add a "click" event listener to the button element 
+//  you created. On click, remove the <main> element node from 
+//  the page. After clicking this button, the user should only 
+//  see the button on the page and the DOM tree should look like this:
+
+
+
+
+
+// Stretch Goals:
+
+// 1. Stretch Goal: Add a text input and a button which, on click, can 
+//  REMOVE an element based on a CSS selector provided by the user in the textbox.
+
+//  2. Stretch Goal: Add a text input and a button which, on click, can CREATE 
+//  an element and append it to the page. This element should contain text 
+//  content provided by the user in the textbox.
